c7d211483ea324bcd05e0bf65fec36cd283c0842
rajasaroj <raja.saroj010@gmail.com>
2023-10-16 17:46:03 +0530

    added login and details

--- src/main/java/com/example/KafkaProducerAzure/config/KafkaConfig.java
+++ src/main/java/com/example/KafkaProducerAzure/config/KafkaConfig.java
@@ -2,6 +2,8 @@ package com.example.KafkaProducerAzure.config;
 
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,12 +16,18 @@ import java.util.Map;
 
 @Configuration
 public class KafkaConfig {
+
+    private static final Logger logger =  LoggerFactory.getLogger(KafkaConfig.class);
+
+
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapServers;
 
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
+        logger.info("-----------Printing Bootstrapservers");
+        logger.info(bootstrapServers);
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
--- src/main/java/com/example/KafkaProducerAzure/service/KafkaProducerService.java
+++ src/main/java/com/example/KafkaProducerAzure/service/KafkaProducerService.java
@@ -1,11 +1,16 @@
 package com.example.KafkaProducerAzure.service;
 
+import com.example.KafkaProducerAzure.config.KafkaConfig;
 import com.example.KafkaProducerAzure.model.TmdMessage;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import jakarta.annotation.PostConstruct;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.core.type.TypeReference;
 
@@ -14,9 +19,12 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class KafkaProducerService {
+
+    private static final Logger logger =  LoggerFactory.getLogger(KafkaProducerService.class);
     @Value("${kafka.topic.name}")
     private String topicName;
 
@@ -36,6 +44,7 @@ public class KafkaProducerService {
     }
 
     public void publishFileMessages() {
+        showKafkaTemplateProperties();
         try (InputStream inputStream = getClass().getResourceAsStream("/data.xml")) {
             List<TmdMessage> messages = xmlMapper.readValue(inputStream, new TypeReference<>() {});
             for (TmdMessage message : messages) {
@@ -45,6 +54,19 @@ public class KafkaProducerService {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void showKafkaTemplateProperties() {
+        ProducerFactory<String, String> producerFactory = kafkaTemplate.getProducerFactory();
+
+        // Access producer properties
+        Map<String, Object> producerConfig = producerFactory.getConfigurationProperties();
+        String bootstrapServers = (String) producerConfig.get(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG);
+        String acks = (String) producerConfig.get(ProducerConfig.ACKS_CONFIG);
+
+        // Log or use the properties as needed
+        logger.info("Bootstrap Servers: " + bootstrapServers);
+        logger.info("Acknowledgment Mode: " + acks);
     }
 
 }
